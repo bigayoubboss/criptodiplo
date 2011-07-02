3d2541e4c82b496227dd216d48a5a99de06f22e7
Christian Rodriguez <Christian Rodriguez@localhost>
2011-07-02 01:41:29 -0500

    Changed look of web pages

--- src/src/banerand.js
+++ src/src/banerand.js
@@ -1,23 +1,23 @@
 function RandomNumber() {
-  var today = new Date();
-  var num= Math.abs(Math.sin(today.getTime()/1000));
-  return num;
+    var today = new Date();
+    var num= Math.abs(Math.sin(today.getTime()/1000));
+    return num;
 }
 
 function RotativosAvisos() 
 {
-  var x = RandomNumber();
+    var x = RandomNumber();
 
-  if (x > .80){
-     document.write("<img src=images/collage_superior.jpg alt=Facultad_Ciencias border=0 align=center>");
-     return; 
-}
-  if (x > .40){
-     document.write("<img src=images/collage_superior2.jpg alt=Facultad_Ciencias border=0 align=center>");
-     return; 
-}
-  if (x > .0){
-     document.write("<img src=images/collage_superior3.jpg alt=Facultad_Ciencias border=0 align=center>");
-     return; 
-  }
+    if (x > .80){
+        document.write("<img src=images/collage_superior.jpg alt=Facultad_Ciencias border=0 align=center>");
+        return; 
+    }
+    if (x > .40){
+        document.write("<img src=images/collage_superior2.jpg alt=Facultad_Ciencias border=0 align=center>");
+        return; 
+    }
+    if (x > .0){
+        document.write("<img src=images/collage_superior3.jpg alt=Facultad_Ciencias border=0 align=center>");
+        return; 
+    }
 }
